a44f1f729de5db69b9027f41c3d48b438d330794
Dru Lang <drulang@gmail.com>
2013-03-16 16:52:18 -0400

    Initial code

---
+++ layout.php
@@ -0,0 +1,24 @@
+<?php
+
+    function get_head()
+    {
+        return '
+<head>
+    <meta charset="utf-8">
+    <title>All things Dru</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Dru Lang, Dru Christian Lang, Programmer, Code, Open Source, Github">
+    <meta name="author" content="Dru Lang">
+
+    <!-- Le styles -->
+    <link href="./css/bootstrap.css" rel="stylesheet">
+    <style>
+    body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+    }
+    </style>
+    <link href="./css/bootstrap-responsive.css" rel="stylesheet">
+</head>';
+    }
+
+?>
