725f0cea3d250f721b815fbd29884080889731a4
Dru Lang <drulang@gmail.com>
2013-06-24 20:20:23 -0400

    Updated head nav

--- layout.php
+++ layout.php
@@ -1,24 +1,93 @@
 <?php
 
+    $GLOBALS['docroot'] = "http://".$_SERVER['SERVER_NAME'];
+
     function get_head()
     {
         return '
 <head>
     <meta charset="utf-8">
-    <title>All things Dru</title>
+    <title>drulang</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Dru Lang, Dru Christian Lang, Programmer, Code, Open Source, Github">
+    <meta name="description" content="Dru Lang, Dru Christian Lang, Programmer, Code, Coder, Freelance, Freelance Programmer, Programming">
     <meta name="author" content="Dru Lang">
 
     <!-- Le styles -->
-    <link href="./css/bootstrap.css" rel="stylesheet">
+    <link href="'.$GLOBALS['docroot'].'/css/bootstrap.css" rel="stylesheet">
     <style>
     body {
         padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
     }
     </style>
-    <link href="./css/bootstrap-responsive.css" rel="stylesheet">
+    <link href= '.$GLOBALS['docroot'].'/css/bootstrap-responsive.css" rel="stylesheet">
 </head>';
     }
 
+    function get_top_navbar()
+    {
+        return '
+<div class="navbar navbar-inverse navbar-fixed-top">
+<div class="navbar-inner">
+    <div class="container">
+
+        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+
+        <a class="brand" href="'.$GLOBALS['docroot'].'">dru</a>
+
+        <div class="nav-collapse collapse">
+            <ul class="nav">
+                <li class="dropdown">
+                    <a href="'.$GLOBALS['docroot'].'/drulang_resume_2013.pdf">Resume</a>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="'.$GLOBALS['docroot'].'/freelance.php">Freelance<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/freelance.php">General</a></li>
+                        <li><a href="/freelance.php?tbl=programming">Programming</a></li>
+                        <li><a href="/freelance.php?tbl=db">Database Design</a></li>
+                        <li><a href="/freelance.php?tbl=automation">Automation</a></li>
+                        <li><a href="/freelance.php?tbl=architecture">Architetures</a></li>
+                        <li><a href="/freelance.php?tbl=quote">Quote</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Products</a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Home Lab</a></li>
+                        <li><a href="#">NoSQL</a></li>
+                        <li><a href="/sixpacksinday/">Six Pack Sinday</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+</div>';
+    }
+
+    
+    function get_js()
+    {
+        return '<!-- Placed at the end of the document so the pages load faster -->
+        <script src="'.$GLOBALS['docroot'].'/js/jquery.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-transition.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-alert.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-modal.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-dropdown.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-scrollspy.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-tab.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-tooltip.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-popover.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-button.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-collapse.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-carousel.js"></script>
+        <script src="'.$GLOBALS['docroot'].'/js/bootstrap-typeahead.js"></script>';
+    }
 ?>
